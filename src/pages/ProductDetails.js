859a1e13ab167fd2e60ea7996038bd270238ac2a
Pannu786 <thepannu786@gmail.com>
2022-02-04 22:47:56 +0530

    added dynamic routes for pro details

---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+  return (
+    <section>
+      <h1>The Products Details Page</h1>
+    </section>
+  );
+};
+
+export default ProductDetails;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A Book 1</li>
+        <li>A Candy 2</li>
+        <li>A Sandy 3</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
