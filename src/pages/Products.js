9c651b6a72495edbf5f0ac4d0eb93b71d8dce531
Pannu786 <thepannu786@gmail.com>
2022-02-05 11:59:12 +0530

    switch and exact added

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Book 1</li>
-        <li>A Candy 2</li>
-        <li>A Sandy 3</li>
+        <li>
+          <Link to='/products/p1'>A Book 1</Link>
+        </li>
+        <li>
+          <Link to='/products/p2'>A Candy 2</Link>
+        </li>
+        <li>
+          <Link to='/products/p3'>A Sandy 3</Link>
+        </li>
       </ul>
     </section>
   );
